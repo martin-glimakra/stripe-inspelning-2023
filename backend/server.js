0bc2536108488dd7756e2b534173cbd7004b78d4
martin-glimakra <88892064+martin-glimakra@users.noreply.github.com>
2023-01-30 17:44:05 +0100

    POST to create order working

--- backend/server.js
+++ backend/server.js
@@ -62,3 +62,62 @@ app.get("/get-all-products", async (req, res) => {
     .catch((error) => {
       console.log(error.response.data);
     });})
+
+
+function createWcOrder () {
+    console.log('createWcOrder körs')
+
+    const data = {
+        payment_method: "bacs",
+        payment_method_title: "Direct Bank Transfer",
+        set_paid: true,
+        billing: {
+          first_name: "John",
+          last_name: "Doe",
+          address_1: "969 Market",
+          address_2: "",
+          city: "San Francisco",
+          state: "CA",
+          postcode: "94103",
+          country: "US",
+          email: "john.doe@example.com",
+          phone: "(555) 555-5555"
+        },
+        shipping: {
+          first_name: "John",
+          last_name: "Doe",
+          address_1: "969 Market",
+          address_2: "",
+          city: "San Francisco",
+          state: "CA",
+          postcode: "94103",
+          country: "US"
+        },
+        line_items: [
+          {
+            product_id: 93,
+            quantity: 2
+          },
+          {
+            product_id: 22,
+            variation_id: 23,
+            quantity: 1
+          }
+        ],
+        shipping_lines: [
+          {
+            method_id: "flat_rate",
+            method_title: "Flat Rate",
+            total: "10.00"
+          }
+        ]
+      };
+
+      WooCommerce.post("orders", data)
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error.response.data);
+  });
+}
