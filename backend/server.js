a74c4ad012ba06b5a0bb4c7c68e4474aabf91d4f
martin-glimakra <88892064+martin-glimakra@users.noreply.github.com>
2023-01-30 17:32:36 +0100

    get all products from WC working

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const WooCommerceRestApi = require("@woocommerce/woocommerce-rest-api").default;
 const express = require("express")
 const app = express()
 const cors = require("cors")
@@ -13,8 +14,15 @@ console.log('app.listen(5000)')
 
 const STRIPE_PRIVATE_KEY = 'sk_test_51KZXQ8Foq0qfjkNLwv2SiG7gYVwkpz878gB5KLJndEDiwCs3ECEh8Uv6rioYChiT1SuNrWDGKCTzjXJ40mUyn7wc00oYnGs75r'
 const CLIENT_URL = 'http://localhost:3000/'
-
 const stripe = require("stripe")(STRIPE_PRIVATE_KEY)
+
+
+const WooCommerce = new WooCommerceRestApi({
+    url: 'https://inspelning.xn--webbutvecklare-glimkra-65b.se/',
+    consumerKey: 'ck_d68f118bdb17951183ca676f915d59274b51b4da',
+    consumerSecret: 'cs_966f5fc9d439f7daa804522f0a5d24dae1c08ccf',
+    version: 'wc/v3'
+  });
 
 app.post("/create-checkout-session", async (req, res) => {
 
@@ -44,3 +52,13 @@ app.post("/create-checkout-session", async (req, res) => {
       res.status(500).json({ error: e.message })
     }
   })
+
+app.get("/get-all-products", async (req, res) => {
+
+    WooCommerce.get("products")
+    .then((response) => {
+      res.json(response.data)
+    })
+    .catch((error) => {
+      console.log(error.response.data);
+    });})
